19450abb2a183cc318c0054983674c8db7e6bbb8
dine <poekie1>
2014-06-27 09:56:50 +0200

    IVR-38 Make CSV report campaign specific.

--- core/src/main/java/org/celllife/ivr/application/utils/FindMessageStatusesProcedure.java
+++ core/src/main/java/org/celllife/ivr/application/utils/FindMessageStatusesProcedure.java
@@ -1,8 +1,10 @@
 package org.celllife.ivr.application.utils;
 
+import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.object.StoredProcedure;
 
 import javax.sql.DataSource;
+import java.sql.Types;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -11,6 +13,9 @@ public class FindMessageStatusesProcedure extends StoredProcedure {
     public FindMessageStatusesProcedure(DataSource dataSource) {
 
         super(dataSource,"FindMessageStatuses");
+        SqlParameter parameter = new SqlParameter("camp",Types.BIGINT);
+        SqlParameter[] parameters = {parameter};
+        this.setParameters(parameters);
         compile();
 
     }
