08f580cedca61f181a75ca1920f71ff5d87bd121
dine <poekie1>
2014-07-03 15:12:04 +0200

    Cosmetic changes.

--- core/src/main/java/org/celllife/ivr/application/utils/FindMessageStatusesProcedure.java
+++ core/src/main/java/org/celllife/ivr/application/utils/FindMessageStatusesProcedure.java
@@ -8,16 +8,21 @@ import java.sql.Types;
 import java.util.Iterator;
 import java.util.Map;
 
+/**
+ * This class is to execute the stored procedure FindMessageStatuses. It has one parameter, the campaign id "camp".
+ */
 public class FindMessageStatusesProcedure extends StoredProcedure {
 
+    /**
+     * This constructor needs to be used in a spring xml file. In this case, see spring-jdbc.xml.
+     * @param dataSource The datasource that contains the stored procedure.
+     */
     public FindMessageStatusesProcedure(DataSource dataSource) {
-
         super(dataSource,"FindMessageStatuses");
         SqlParameter parameter = new SqlParameter("camp",Types.BIGINT);
         SqlParameter[] parameters = {parameter};
         this.setParameters(parameters);
         compile();
-
     }
 
     protected void printMap(Map results) {
@@ -27,10 +32,3 @@ public class FindMessageStatusesProcedure extends StoredProcedure {
     }
 
 }
-
-
-
-
-
-
-
